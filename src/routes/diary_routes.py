97ab184681a7690345edd3e8a010d6acfd8b7652
discovertechapp <discovertechapp@gmail.com>
2026-05-16 22:54:12 +0530

    Add diary route to diary_routes.py

--- src/routes/diary_routes.py
+++ src/routes/diary_routes.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
 
 diary_bp = Blueprint("diary_bp", __name__)
+
+
+@diary_bp.route("/diary")
+def diary():
+    return {
+        "status": True,
+        "message": "Diary Route Working"
+    }
