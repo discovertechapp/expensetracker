401b4eed322069ca45b752faff9be4c6af62b190
discovertechapp <discovertechapp@gmail.com>
2026-05-16 23:37:20 +0530

    Update dashboard_routes.py

--- src/routes/dashboard_routes.py
+++ src/routes/dashboard_routes.py
@@ -4,7 +4,7 @@ from src.services.dashboard_service import (
     get_dashboard_summary,
     get_monthly_summary
 )
-
+from src.utils.auth_middleware import token_required
 dashboard_bp = Blueprint("dashboard_bp", __name__)
 
 
@@ -12,6 +12,7 @@ dashboard_bp = Blueprint("dashboard_bp", __name__)
 # Dashboard Summary
 # ---------------------------------------------------------
 @dashboard_bp.route("/dashboard", methods=["GET"])
+@token_required
 def dashboard():
 
     user_id = request.args.get("user_id")
@@ -28,6 +29,7 @@ def dashboard():
     "/dashboard/monthly-summary",
     methods=["GET"]
 )
+@token_required
 def monthly_summary():
 
     user_id = request.args.get("user_id")
