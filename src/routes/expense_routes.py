42cd3440d4da7b018aed79ab4d12afeca9eac47a
discovertechapp <discovertechapp@gmail.com>
2026-05-16 22:53:34 +0530

    Update expense_routes.py

--- src/routes/expense_routes.py
+++ src/routes/expense_routes.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
 
 expense_bp = Blueprint("expense_bp", __name__)
+
+
+@expense_bp.route("/expenses")
+def expenses():
+    return {
+        "status": True,
+        "message": "Expense Route Working"
+    }
