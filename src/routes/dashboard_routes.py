880c31a1c4592f628fc1aa5a67b0745ed91cdc96
discovertechapp <discovertechapp@gmail.com>
2026-05-16 23:31:45 +0530

    Update dashboard_routes.py

--- src/routes/dashboard_routes.py
+++ src/routes/dashboard_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
 
 from src.services.dashboard_service import (
-    get_dashboard_summary
+    get_dashboard_summary,
+    get_monthly_summary
 )
 
 dashboard_bp = Blueprint("dashboard_bp", __name__)
@@ -16,5 +17,21 @@ def dashboard():
     user_id = request.args.get("user_id")
 
     response = get_dashboard_summary(user_id)
+
+    return response
+
+
+# ---------------------------------------------------------
+# Monthly Summary
+# ---------------------------------------------------------
+@dashboard_bp.route(
+    "/dashboard/monthly-summary",
+    methods=["GET"]
+)
+def monthly_summary():
+
+    user_id = request.args.get("user_id")
+
+    response = get_monthly_summary(user_id)
 
     return response
