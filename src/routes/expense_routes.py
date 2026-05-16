020247657beb18f757aba0920b163fcc10315510
discovertechapp <discovertechapp@gmail.com>
2026-05-16 23:38:30 +0530

    Implement expense search endpoint
    Added a new endpoint for searching expenses based on user ID, main category, and date range.

--- src/routes/expense_routes.py
+++ src/routes/expense_routes.py
@@ -4,7 +4,8 @@ from src.services.expense_service import (
     create_expense,
     get_expenses,
     update_expense,
-    delete_expense
+    delete_expense,
+    search_expenses
 )
 from src.utils.auth_middleware import token_required
 expense_bp = Blueprint("expense_bp", __name__)
@@ -69,5 +70,39 @@ def edit_expense(document_id):
 def remove_expense(document_id):
 
     response = delete_expense(document_id)
+
+    return response
+
+
+# ---------------------------------------------------------
+# Search Expenses
+# ---------------------------------------------------------
+@expense_bp.route(
+    "/expenses/search",
+    methods=["GET"]
+)
+@token_required
+def expense_search():
+
+    user_id = request.args.get("user_id")
+
+    main_category = request.args.get(
+        "main_category"
+    )
+
+    start_date = request.args.get(
+        "start_date"
+    )
+
+    end_date = request.args.get(
+        "end_date"
+    )
+
+    response = search_expenses(
+        user_id,
+        main_category,
+        start_date,
+        end_date
+    )
 
     return response
