28e7ac6e9a1d7e159c244fee2c037e9f5359344f
discovertechapp <discovertechapp@gmail.com>
2026-05-16 22:48:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask
 
 from src.routes.auth_routes import auth_bp
-from src.routes.expense_routes import expense_bp
-from src.routes.dashboard_routes import dashboard_bp
-from src.routes.diary_routes import diary_bp
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'expense_tracker_secret'
 
+# Register Blueprints
 app.register_blueprint(auth_bp)
-app.register_blueprint(expense_bp)
-app.register_blueprint(dashboard_bp)
-app.register_blueprint(diary_bp)
+
+
+@app.route("/")
+def home():
+    return {
+        "status": True,
+        "message": "Expense Tracker Running Successfully"
+    }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
