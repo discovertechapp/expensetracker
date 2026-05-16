8497d45a9bed68a819cd84e9b83f10c70a95d204
discovertechapp <discovertechapp@gmail.com>
2026-05-16 23:01:40 +0530

    Update auth_routes.py

--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 
 from src.services.auth_service import register_user
 from src.services.auth_service import login_user
+from src.services.auth_service import approve_user
 
 auth_bp = Blueprint("auth_bp", __name__)
 
@@ -23,6 +24,14 @@ def register():
     payload = request.json
 
     response = register_user(payload)
+
+    return response
+
+
+@auth_bp.route("/approve-user/<int:user_id>", methods=["PUT"])
+def approve(user_id):
+
+    response = approve_user(user_id)
 
     return response
 
