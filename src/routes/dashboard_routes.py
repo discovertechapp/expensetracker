16e77bef7a39371177c8e313edf087e8904496e2
discovertechapp <discovertechapp@gmail.com>
2026-05-16 22:53:53 +0530

    Update dashboard_routes.py

--- src/routes/dashboard_routes.py
+++ src/routes/dashboard_routes.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
 
 dashboard_bp = Blueprint("dashboard_bp", __name__)
+
+
+@dashboard_bp.route("/dashboard")
+def dashboard():
+    return {
+        "status": True,
+        "message": "Dashboard Route Working"
+    }
