e813aac5c1c6519c2100faf1dd09cc433d9098f5
discovertechapp <discovertechapp@gmail.com>
2026-05-16 23:26:33 +0530

    Update diary_routes.py

--- src/routes/diary_routes.py
+++ src/routes/diary_routes.py
@@ -1,11 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, request
+
+from src.services.diary_service import (
+    create_diary_entry,
+    get_diary_entries
+)
 
 diary_bp = Blueprint("diary_bp", __name__)
 
 
-@diary_bp.route("/diary")
+# ---------------------------------------------------------
+# Create Diary Entry
+# ---------------------------------------------------------
+@diary_bp.route("/diary", methods=["POST"])
+def add_diary():
+
+    payload = request.json
+
+    response = create_diary_entry(payload)
+
+    return response
+
+
+# ---------------------------------------------------------
+# Get Diary Entries
+# ---------------------------------------------------------
+@diary_bp.route("/diary", methods=["GET"])
 def diary():
-    return {
-        "status": True,
-        "message": "Diary Route Working"
-    }
+
+    user_id = request.args.get("user_id")
+
+    response = get_diary_entries(user_id)
+
+    return response
