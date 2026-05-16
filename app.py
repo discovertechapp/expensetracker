2d4718388df5d3d0ab2c1c59df1c3e2e51e473c5
discovertechapp <discovertechapp@gmail.com>
2026-05-16 23:16:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from src.routes.auth_routes import auth_bp
 from src.routes.expense_routes import expense_bp
 from src.routes.dashboard_routes import dashboard_bp
 from src.routes.diary_routes import diary_bp
+
+from src.utils.index_manager import create_indices
 from src.services.auth_service import create_default_admin
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'expense_tracker_secret'
 
+# Register Blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(expense_bp)
 app.register_blueprint(dashboard_bp)
@@ -18,6 +21,7 @@ app.register_blueprint(diary_bp)
 
 @app.route("/")
 def home():
+
     return {
         "status": True,
         "message": "Expense Tracker Running Successfully"
@@ -25,8 +29,13 @@ def home():
 
 
 if __name__ == "__main__":
+
+    # Create Elasticsearch indices
+    create_indices()
+
+    # Create default admin
     create_default_admin()
-    
+
     app.run(
         debug=True,
         host="0.0.0.0",
