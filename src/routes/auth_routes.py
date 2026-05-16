cb77cc768557a44d98099af192cfb6af6c9524a7
discovertechapp <discovertechapp@gmail.com>
2026-05-16 22:55:35 +0530

    Update auth_routes.py

--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -1,11 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
+
+from src.services.auth_service import register_user
 
 auth_bp = Blueprint("auth_bp", __name__)
 
 
 @auth_bp.route("/login")
 def login():
+
     return {
         "status": True,
         "message": "Login Route Working"
     }
+
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+
+    payload = request.json
+
+    response = register_user(payload)
+
+    return response
